4a83ebb7ba94e8f405b61ca7d7082f974ccc8e36
Marc Guillem <magusan.1970@gmail.com>
2024-06-15 23:42:08 +0200

    Adding more tests

--- src/app/components/config/config.component.spec.ts
+++ src/app/components/config/config.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ConfigComponent } from './config.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfigComponent } from './config.component';
+import { LMSTudioAPIConfiguration } from '../../interface/configuration';
 
 describe('ConfigComponent', () => {
   let component: ConfigComponent;
@@ -10,15 +10,12 @@ describe('ConfigComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        ConfigComponent,
-        BrowserAnimationsModule
-      ],
+      declarations: [],
+      imports: [ConfigComponent, BrowserAnimationsModule],
       providers: [
-        { provide: MatDialogRef, useValue: {} }
-      ]
-    })
-      .compileComponents();
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) }
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfigComponent);
     component = fixture.componentInstance;
@@ -27,5 +24,22 @@ describe('ConfigComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should reset formGroup to default configuration', () => {
+    component.resetToDefaults();
+    expect(component.formGroup.value).toEqual(component["configService"]._defaultConfiguration);
+  });
+
+  it('should set configuration and close dialog', () => {
+    const reloadModelsAfterCloseThisModal = true;
+    component.formGroup.setValue({
+      host: 'example.com',
+      systemPrompt: 'Hello',
+    });
+    spyOn(component["configService"], "setConfiguration");
+    component.setConfiguration(reloadModelsAfterCloseThisModal);
+    expect(component["configService"].setConfiguration).toHaveBeenCalledWith(component.formGroup.value as LMSTudioAPIConfiguration);
+    expect(component["matDialogRef"].close).toHaveBeenCalledWith(reloadModelsAfterCloseThisModal);
   });
 });
